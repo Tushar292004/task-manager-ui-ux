c88d34be7b1fee5b16b2aecdeff9dde19318f920
TUSHAR292004 <tusharchandak2004@gmail.com>
2025-02-06 18:36:12 +0530

    Wrike

--- task-manager-ui/src/components/Header.tsx
+++ task-manager-ui/src/components/Header.tsx
@@ -1,7 +1,7 @@
 import { LineShadowText } from "./line-shadow-text";
 const Header = () => {
     return (
-        <div className=" px-10 border-b-2 border-[#BA532D] bg-muted/50 py-4 flex justify-between">
+        <div className=" px-4 border-b-2 border-[#BA532D] bg-muted/50 py-4 flex justify-between">
             <div className="container mx-auto  flex justify-between items-center">
                 <LineShadowText  shadowColor="#BA555D" className=" text-4xl  text-[#BA532D] font-bold tracking-tight ">Wrike.</LineShadowText>
             </div>
