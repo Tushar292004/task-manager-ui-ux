336e1eb77d28357b57b74a6fc25e5f11195e67ec
Tushar Chandak <tusharchandak2004@gmail.com>
2025-02-24 14:55:41 +0530

    task-manager

--- task-manager-ui/src/components/Footer.tsx
+++ task-manager-ui/src/components/Footer.tsx
@@ -4,10 +4,10 @@ const Footer = () => {
     return (
       <div className="px-8 items-center flex py-3 flex-col md-flex-row w-full justify-between align-middle 
 border-t-2 bg-muted/50 border-t-[#BA532D]" >
-        <div className="flex flex-row gap-4 items-start text-[#BA532D] ">
-        <Github  className="hover:text-white"/>
-        <Linkedin  className="hover:text-white"/>
-        <Instagram className="hover:text-white" />
+        <div className="flex flex-row gap-4 items-start ">
+        <Github  className="hover:text-[#BA532D] "/>
+        <Linkedin  className="hover:text-[#BA532D] "/>
+        <Instagram className="hover:text-[#BA532D] " />
         </div>
           <div className="">
               <span className="text-[#BA532D] text-2xl font-medium tracking-tighter">
